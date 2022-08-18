cb8ef6ffbe8480097a57f8f25bd692fb3931f359
yoyohua <yoyohua1230@gmail.com>
2022-08-19 00:17:20 +0800

    workpreview5

---
+++ js/job.js
@@ -0,0 +1,65 @@
+// JavaScript Document
+
+/*職業介紹*/
+
+	$(function(){
+		// 預設顯示第一個 Tab
+		var _showTab = 0;
+		$('.news_tab').each(function(){
+			// 目前的頁籤區塊
+			var $sp = $(this);
+
+			var $defaultLi = $('ul.sps li', $sp).eq(_showTab).addClass('active');
+			$($defaultLi.find('a').attr('href')).siblings().hide();
+			
+			// 當 li 頁籤被點擊時...
+			// 若要改成滑鼠移到 li 頁籤就切換時, 把 click 改成 mouseover
+			$('ul.sps li', $sp).click(function() {
+				// 找出 li 中的超連結 href(#id)
+				var $this = $(this),
+					_clickTab = $this.find('a').attr('href');
+				// 把目前點擊到的 li 頁籤加上 .active
+				// 並把兄弟元素中有 .active 的都移除 class
+				$this.addClass('active').siblings('.active').removeClass('active');
+				// 淡入相對應的內容並隱藏兄弟元素
+				$(_clickTab).stop(false, true).fadeIn().siblings().hide();
+
+				return false;
+			}).find('a').focus(function(){
+				this.blur();
+			});
+		});
+	});
+
+
+/*01*/
+	$(function(){
+		$('.news_tab').each(function(){
+			$('.sp-01').click(function(){
+					$('#sp2').hide();
+                    $('#sp3').hide();
+                    $('#sp4').hide();
+					$('#sp1').show();
+				});
+			$('.sp-02').click(function(){
+				$('#sp2').show();
+				$('#sp1').hide();
+                $('#sp3').hide();
+                $('#sp4').hide();
+				});
+            
+            $('.sp-03').click(function(){
+                    $('#sp1').hide();
+					$('#sp2').hide();
+					$('#sp3').show();
+                    $('#sp4').hide();
+				});
+            
+            $('.sp-04').click(function(){
+                    $('#sp1').hide();
+					$('#sp2').hide();
+					$('#sp3').hide();
+                    $('#sp4').show();
+				});
+			});
+});
